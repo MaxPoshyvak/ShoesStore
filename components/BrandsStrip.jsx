982f0c0e03ca6379dacaa4a715445ef7b1200584
Sviatoslav Tymtsiv <svtm1234567@gmail.com>
2026-04-11 15:07:01 +0300

    correction ajio

--- components/BrandsStrip.jsx
+++ components/BrandsStrip.jsx
@@ -9,19 +9,19 @@ export default function BrandsStrip() {
                 <div className={styles.logoGroup}>
                     <Image src="/ebay.svg" alt="Brand 1" width={100} height={50} />
                     <Image src="/amazon.svg" alt="Brand 2" width={100} height={50} />
-                    <Image src="/Ajio.svg" alt="Brand 3" width={100} height={50} />
+                    <Image src="/AJIO.svg" alt="Brand 3" width={100} height={50} />
                     <Image src="/ebay.svg" alt="Brand 1" width={100} height={50} />
                     <Image src="/amazon.svg" alt="Brand 2" width={100} height={50} />
-                    <Image src="/Ajio.svg" alt="Brand 3" width={100} height={50} />
+                    <Image src="/AJIO.svg" alt="Brand 3" width={100} height={50} />
                 </div>
 
                 <div aria-hidden="true" className={styles.logoGroup}>
                     <Image src="/ebay.svg" alt="Brand 1" width={100} height={50} />
                     <Image src="/amazon.svg" alt="Brand 2" width={100} height={50} />
-                    <Image src="/Ajio.svg" alt="Brand 3" width={100} height={50} />
+                    <Image src="/AJIO.svg" alt="Brand 3" width={100} height={50} />
                     <Image src="/ebay.svg" alt="Brand 1" width={100} height={50} />
                     <Image src="/amazon.svg" alt="Brand 2" width={100} height={50} />
-                    <Image src="/Ajio.svg" alt="Brand 3" width={100} height={50} />
+                    <Image src="/AJIO.svg" alt="Brand 3" width={100} height={50} />
                 </div>
                
             </div>
